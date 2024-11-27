c01df2e8a7a9d618488ac77725f08f7c51e0ff1f
Geovani Passos <geovanipassos95@outrlook.com>
2024-11-27 09:23:12 -0300

    Iniciando parte da api, criação do projeto base

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,10 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable, of } from 'rxjs';
+import { tap, take, first, delay, catchError} from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -8,16 +13,41 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    {_id: '1', name:'Angular', category: 'Front-end'}
-  ];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  //coursesService: CoursesService;
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog ){
     //this.courses = [];
+    /*
+    this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+    */
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Error ao carregar a lista de cursos.');
+          return of([])
+        }),
+        take(1),
+        //delay(500),
+        // or first(),
+        tap(courses => console.log(courses))
+      );
+
+      //subscribe manual
+      //this.coursesService.list().subriscribe(courses => this.courses = courses);
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
   }
-
 }
---
+++ crud-spring-api/src/main/java/com/loiane_cursos/crud_spring_api/CrudSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.loiane_cursos.crud_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApiApplication.class, args);
+	}
+
+}
