4355769f36cd04cb8c890e50551e7ca1a2d2a5ed
Geovani Passos <geovanipassos95@outrlook.com>
2024-11-27 10:06:17 -0300

    Criação do model, controller  e repository de course e testado sem o banco de dados a api

--- crud-spring-api/src/main/java/com/loiane_cursos/crud_spring_api/CrudSpringApiApplication.java
+++ crud-spring-api/src/main/java/com/loiane_cursos/crud_spring_api/CrudSpringApiApplication.java
@@ -1,13 +1,35 @@
 package com.loiane_cursos.crud_spring_api;
 
+import com.loiane_cursos.crud_spring_api.model.Course;
+import com.loiane_cursos.crud_spring_api.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApiApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategoria("front-end");
+
+			Course c1 = new Course();
+			c1.setName("Spring Boot");
+			c1.setCategoria("Back-end");
+
+			courseRepository.save(c);
+			courseRepository.save(c1);
+		};
 	}
 
 }
---
+++ crud-spring-api/src/main/java/com/loiane_cursos/crud_spring_api/controller/CourseController.java
@@ -0,0 +1,27 @@
+package com.loiane_cursos.crud_spring_api.controller;
+
+import com.loiane_cursos.crud_spring_api.model.Course;
+import com.loiane_cursos.crud_spring_api.repository.CourseRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+//@AllArgsConstructor Lombok para criar o construtor
+public class CourseController {
+
+    //@Autowired, controller ou setter para criar o construtor
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
